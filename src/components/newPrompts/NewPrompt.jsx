7852404b9b8789628ce319abad75c144865a3076
Swag-blips <coderblip@gmail.com>
2024-10-03 20:04:49 +0100

    chatpage design complete

---
+++ src/components/newPrompts/NewPrompt.jsx
@@ -0,0 +1,26 @@
+import React, { useRef } from "react";
+import "./NewPrompt.css";
+const NewPrompt = () => {
+  const endRef = useRef(null);
+
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+  return (
+    <>
+      <div className="endChat" ref={endRef}></div>
+      <form action="" className="newForm">
+        <label htmlFor="file">
+          <img src="/attachment.png" alt="" />
+        </label>
+        <input id="file" type="file" multiple={false} hidden />
+        <input type="text" placeholder="Ask anything" />
+        <button>
+          <img src="/arrow.png" alt="" />
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default NewPrompt;
--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import "./Chat.css";
+import NewPrompt from "../../components/newPrompts/NewPrompt";
 
 const Chat = () => {
-  return <div>chats</div>;
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="messager">Test messager from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="messager">Test messager from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="messager">Test messager from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="messager">Test messager from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="messager">Test messager from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="messager">Test messager from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="messager">Test messager from ai</div>
+          <div className="message user">Test message from user</div>
+          <div className="message">Test message</div>
+          <div className="message user">Test message from user</div>
+          <div className="messager">Test messager from ai</div>
+          <div className="message user">Test message from user</div>
+          <NewPrompt />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
