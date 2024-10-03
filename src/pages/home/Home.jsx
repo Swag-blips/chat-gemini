badf9cbbc12c6208c20f6d0effad828ebb93e3bb
Swag-blips <coderblip@gmail.com>
2024-10-03 15:21:28 +0100

    home-page added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="homepage">
-      <Link to="/dashboard">Dashboard</Link>
+      <img src="/orbital.png" alt="orbital-img" className="orbital" />
+      <div className="left">
+        <h1>LAMA AI</h1>
+        <h2>Supercharge your creativity and productivity</h2>
+        <h3>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          reiciendis id neque maxime quas voluptatem hic molestiae est nobis
+          iure laboriosam pariatur, aperiam aspernatur, recusandae vero!
+          Consequatur optio illo sapiente.
+        </h3>
+        <Link to="/dashboard">Get Started</Link>
+      </div>
+      <div className="right">
+        <figure className="imgContainer">
+          <div className="bgContainer">
+            <div className="bg"></div>
+          </div>
+          <img src="/bot.png" alt="" className="bot" />
+        </figure>
+      </div>
     </div>
   );
 };
